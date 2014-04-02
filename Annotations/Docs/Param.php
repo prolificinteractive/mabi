b429cba9bf9fdfd713b094d116377dd00d71d831
Kenya Stewart <kenya@prolificinteractive.com>
2014-04-02 17:13:24 -0400

    adding exception for enums instead

--- Annotations/Docs/Param.php
+++ Annotations/Docs/Param.php
@@ -40,19 +40,35 @@ class Param {
   public $description;
 
   function __construct(array $values) {
-    if (empty($values['value'])) {
-      throw AnnotationException::requiredError("value", "Docs\\Param", 'class...expects', "a(n) string.");
-    }
-    else {
-      $this->value = $values['value'];
-    }
-
     if (!empty($values['description'])) {
       $this->description = $values['description'];
     }
 
-    $this->type     = empty($values['type']) ? 'string' : $values['type'];
-    $this->location = empty($values['location']) ? 'body' : $values['location'];
+    if (empty($values['type'])) {
+      $this->type = 'string';
+    }
+    else {
+      $available = array('string', 'int', 'float', 'bool', 'file');
+      $this->setProperty('type', $values['type'], $available);
+    }
+
+    if (empty($values['location'])) {
+      $this->location = 'body';
+    }
+    else {
+      $available = array('url', 'body', 'header', 'query');
+      $this->setProperty('location', $values['location'], $available);
+    }
+
     $this->required = empty($values['required']) ? FALSE : $values['required'];
+  }
+
+  public function setProperty($property, $property_value, $available) {
+    if (!in_array($property_value, $available)) {
+      throw AnnotationException::enumeratorError('type', 'Docs\\Param', 'class...', $available, $property_value);
+    }
+    else {
+      $this->{$property} = $property_value;
+    }
   }
 }
